ea76e46e23a96c7dc9efd09d2618a71c397b93bf
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-13 14:43:09 +0100

    It's a mess and many things are missing (front and links), but it works

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORY = %w[chinese italian japanese french belgian]
+  CATEGORY = %w[indian chinese italian japanese french belgian]
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORY }
